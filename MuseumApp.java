a9e81ccf3f03214d2289be520fc79d7801885759
kanav12344 <kanav_wadhwani@aw.org>
2025-02-12 10:00:30 -0800

    Add files via upload

---
+++ MuseumApp.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MuseumApp {
+	public static void main(String[] args) {
+		
+		Jframe museumframe = new Jframe("Virtual Museum")
+
+		String[] imagefiles = {"images1.jpg", "image2.jpg", "image3.jpg"};
+
+
+		for (int i = 0; 1 < 3; i++) {
+
+			String imagefile = imagefiles;
+
+			public void actionperformed(actionevent e) {
+
+			    showImage( imagefile); 
+			}
+		}
+
+
+         museamframe.add(button);
+	}
+
+
+	  
+}
