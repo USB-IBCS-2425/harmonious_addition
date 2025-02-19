0af9dcb05c624461f9a76f18e87ca231ee0c4972
kanav12344 <kanav_wadhwani@aw.org>
2025-02-19 14:30:51 -0800

    Add files via upload

--- ImageEditing.java
+++ ImageEditing.java
@@ -20,6 +20,8 @@ class ImageEditing {
     public JButton blurButton;
     public JFrame f;
     public JPanel p;
+    public JLabel lab;
+
 
 
 
@@ -36,10 +38,185 @@ class ImageEditing {
         imageButton = new JButton("image");
         imageButton.setActionCommand("IMAGE");
         imageButton.addActionListener(new ButtonClickListener());
-        startframe.add(imageButton);
+        startFrame.add(imageButton);
+
+
+        resetButton = new JButton("reset");
+        resetButton.setActionCommand("RESET");
+        resetButton.addActionListener(new ButtonClickListener());
+        startFrame.add(resetButton);
+
+
+        highlightgButton = new JButton("highlight green");
+        highlightgButton.setActionCommand("HIGHLIGHTGREEN");
+        highlightgButton.addActionListener(new ButtonClickListener());
+        startFrame.add(highlightgButton);
 
 
 
-        
+        contrastButton = new JButton("contrast");
+        contrastButton.setActionCommand("CONTRAST");
+        contrastButton.addActionListener(new ButtonClickListener());
+        startFrame.add(contrastButton);
+
+
+
+        zoomButton = new JButton("zoom");
+        zoomButton.setActionCommand("Zoom");
+        zoomButton.addActionListener(new ButtonClickListener());
+        startFrame.add(zoomButton);
+
+
+
+        rotateButton = new JButton("rotate");
+        rotateButton.setActionCommand("ROTATE");
+        rotateButton.addActionListener(new ButtonClickListener());
+        startFrame.add(rotateButton);
+
+
+
+
+        blurButton = new JButton("blur");
+        blurButton.setActionCommand("BLUR");
+        blurButton.addActionListener(new ButtonClickListener());
+        startFrame.add(blurButton);
+
+
+
+
+         icon = new ImageIcon("filter image.jpg");
+        try {
+        	im = ImageIO.read(new File("filter image.jpg"));
+
+        	 }
+
+        catch(IOException e) {
+        	System.out.println("Error reading image: " + e.getMessage());
+        }
+
+        f = new JFrame();
+		p = new JPanel();
+		lab = new JLabel(icon);
+
+
+		public static void main(String[] args) {
+		ImageEditing mWin = new ImageEditing();
+	    }
+
+
+	    private class ButtonClickListener implements ActionListener{
+            public void actionPerformed(ActionEvent e) {
+                String command = e.getActionCommand();
+
+
+
+                 if(command.equals("IMAGE"))  {
+				f.add(p);
+				p.add(image);
+				f.pack();
+				f.setVisible(true);
+
+
+				int width = im.getWidth();
+         		int height = im.getHeight();
+   
+         		for (int i = 0; i < width; i++) {
+         			for (int j = 0; j < height; j++) {
+
+
+         		if(command.equals("RESET"))  {
+
+				int width = im.getWidth();
+         		int height = im.getHeight();
+
+         		for (int i = 0; i < width; i++) {
+         			for (int j = 0; j < height; j++) {
+
+
+
+
+         		if(command.equals("HIGHLIGHTGREEN"))  {
+
+				int width = im.getWidth();
+         		int height = im.getHeight();
+
+         		for (int i = 0; i < width; i++) {
+         			for (int j = 0; j < height; j++) {
+
+
+
+
+
+         		if(command.equals("CONTRAST"))  {
+
+				int width = im.getWidth();
+         		int height = im.getHeight();
+
+         		for (int i = 0; i < width; i++) {
+         			for (int j = 0; j < height; j++) {
+
+
+
+
+
+         		if(command.equals("ZOOM"))  {
+
+				int width = im.getWidth();
+         		int height = im.getHeight();
+
+         		for (int i = 0; i < width; i++) {
+         			for (int j = 0; j < height; j++) {
+
+
+
+
+
+         		if(command.equals("ROTATE"))  {
+
+				int width = im.getWidth();
+         		int height = im.getHeight();
+
+         		for (int i = 0; i < width; i++) {
+         			for (int j = 0; j < height; j++) {
+
+
+
+
+
+         		if(command.equals("BLUR"))  {
+
+				int width = im.getWidth();
+         		int height = im.getHeight();
+
+         		for (int i = 0; i < width; i++) {
+         			for (int j = 0; j < height; j++) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
